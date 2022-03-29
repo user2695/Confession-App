3046c7982e24504c26fad73471bdb8df2b71eeb6
user2695 <zy.khan37@gmail.com>
2022-03-29 21:26:47 +0530

    Added functioning database

--- app.js
+++ app.js
@@ -1,38 +1,68 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const homeStartingContent = "This is the home page of confetti where people can confess something. Please feel free to leave a confession at by hitting compose button right above. Have fun!!!"
 
 const app = express();
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect("mongodb://localhost:27017/ConfessionDB", {
+    useNewUrlParser: true
+});
+const postSchema = {
+    title: String,
+    content: String
+};
+const Post = mongoose.model("Post", postSchema);
 
 app.get('/', function (req, res) {
-    res.render('home', {
-        posts: posts
-    });
-})
+    Post.find({}, function (err, posts) {
+
+        res.render('home', {
+            startingContent: homeStartingContent,
+            posts: posts
+        });
+    })
+});
 app.get('/compose', function (req, res) {
     res.render("compose.ejs")
 })
 app.get('/home', function (req, res) {
     res.render("home.ejs")
-})
+});
 
 app.post('/compose', function (req, res) {
-    const post = {
-        name: req.body.name,
-        text: req.body.text
-    };
-    posts.push(post);
-    res.redirect('/');
-})
+    const post = new Post({
+        title: req.body.postTitle,
+        content: req.body.postBody
+    });
 
+    post.save(function (err) {
+        if (!err) {
+            res.redirect("/");
+        }
+    });
+});
 
+app.get('/posts/:postId', function (req, res) {
+    const requestedPostId = req.params.postId;
+    Post.findOne({
+        _id: requestedPostId
+    }, function (err, post) {
+        res.render("post", {
+            title: post.title,
+            content: post.content
+        });
+    });
+});
 
 app.listen(3000, function () {
     console.log("Running on: 3000");
